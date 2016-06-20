7c4be4b9e6d92148844e048bb5b6c4705a597837
Chris Cotnoir <ccotnoir@xtuple.com>
2016-06-20 16:20:28 -0400

    Refactored functions to reduce redundancy

--- foundation-database/public/functions/calcpurchaseorderamt.sql
+++ foundation-database/public/functions/calcpurchaseorderamt.sql
@@ -12,46 +12,9 @@ CREATE OR REPLACE FUNCTION calcPurchaseOrderAmt(pPoheadid INTEGER,
                                                 pType TEXT) RETURNS NUMERIC AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-DECLARE
-  _subtotal NUMERIC := 0;
-  _freightsub NUMERIC := 0;
-  _tax NUMERIC := 0;
-  _freight NUMERIC := 0;
-  _currid INTEGER := 0;
-  _effdate DATE := CURRENT_DATE;
-  _amount NUMERIC := 0;
-
 BEGIN
 
-  -- pType: S = line item subtotal
-  --        T = total
-  --        X = tax
-
-  SELECT COALESCE(SUM(ROUND(poitem_qty_ordered * poitem_unitprice, 2)), 0),
-         COALESCE(SUM(ROUND(poitem_freight, 2)), 0)
-         INTO _subtotal, _freightsub
-  FROM poitem
-  WHERE (poitem_pohead_id=pPoheadid);
-
-  SELECT COALESCE(SUM(tax), 0) INTO _tax
-  FROM ( SELECT COALESCE(ROUND(SUM(taxdetail_tax), 2), 0.0) AS tax
-         FROM tax JOIN calculateTaxDetailSummary('PO', pPoheadid, 'T') ON (taxdetail_tax_id=tax_id)
-         GROUP BY tax_id ) AS data;
-
-  SELECT COALESCE(pohead_freight, 0), pohead_curr_id, pohead_orderdate
-         INTO _freight, _currid, _effdate
-  FROM pohead
-  WHERE (pohead_id=pPoheadid);
-
-  _amount := currToBase(_currid,
-                        CASE pType WHEN 'S' THEN (_subtotal)
-                                   WHEN 'T' THEN (_subtotal + _tax + _freight + _freightsub)
-                                   WHEN 'X' THEN (_tax)
-                                   ELSE 0.0
-                        END,
-                        _effdate);
-
-  RETURN _amount;
+  RETURN calcPurchaseOrderAmt(pPoheadid, 'T', NULL, NULL, NULL, NULL, FALSE);
 
 END;
 $$ LANGUAGE 'plpgsql';
@@ -67,7 +30,7 @@ END;
 $$ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION calcPurchaseOrderAmt(pPoheadid INTEGER,
-                                                pType TEXT, pTaxzoneId INTEGER, pOrderDate DATE, pCurrId INTEGER, pFreight NUMERIC) RETURNS NUMERIC AS $$
+                                                pType TEXT, pTaxzoneId INTEGER, pOrderDate DATE, pCurrId INTEGER, pFreight NUMERIC, pQuick BOOLEAN DEFAULT TRUE) RETURNS NUMERIC AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
@@ -93,16 +56,25 @@ BEGIN
 
   SELECT COALESCE(SUM(tax), 0) INTO _tax
   FROM ( SELECT COALESCE(ROUND(SUM(taxdetail_tax), 2), 0.0) AS tax
-         FROM tax JOIN calculateTaxDetailSummary('PO', pPoheadid, 'T', COALESCE(pTaxzoneId, -1), pOrderDate, pCurrId, COALESCE(pFreight,0.0)) ON (taxdetail_tax_id=tax_id)
+         FROM tax JOIN calculateTaxDetailSummary('PO', pPoheadid, 'T', COALESCE(pTaxzoneId, -1), COALESCE(pOrderDate, CURRENT_DATE), pCurrId, COALESCE(pFreight,0.0), pQuick) ON (taxdetail_tax_id=tax_id)
          GROUP BY tax_id ) AS data;
 
-  _amount := currToBase(pCurrId,
+  IF (pQuick) THEN
+    SELECT COALESCE(pFreight, 0), pCurrId, pOrderDate INTO _freight, _currid, _effdate;
+  ELSE
+    SELECT COALESCE(pohead_freight, 0), pohead_curr_id, pohead_orderdate
+           INTO _freight, _currid, _effdate
+    FROM pohead
+    WHERE (pohead_id=pPoheadid);
+  END IF;
+
+  _amount := currToBase(_currId,
                         CASE pType WHEN 'S' THEN (_subtotal)
-                                   WHEN 'T' THEN (_subtotal + _tax + COALESCE(pFreight, 0) + _freightsub)
+                                   WHEN 'T' THEN (_subtotal + _tax + _freight + _freightsub)
                                    WHEN 'X' THEN (_tax)
                                    ELSE 0.0
                         END,
-                        pOrderDate);
+                        _effdate);
 
   RETURN _amount;
 
--- foundation-database/public/functions/calcquoteamt.sql
+++ foundation-database/public/functions/calcquoteamt.sql
@@ -12,51 +12,9 @@ CREATE OR REPLACE FUNCTION calcQuoteAmt(pQuheadid INTEGER,
                                         pType TEXT) RETURNS NUMERIC STABLE AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-DECLARE
-  _subtotal NUMERIC := 0.0;
-  _cost NUMERIC := 0.0;
-  _tax NUMERIC := 0.0;
-  _freight NUMERIC := 0.0;
-  _misc NUMERIC := 0.0;
-  _amount NUMERIC := 0.0;
-
 BEGIN
 
-  -- pType: S = line item subtotal
-  --        T = total
-  --        X = tax
-  --        M = margin
-
-  SELECT COALESCE(SUM(ROUND((quitem_qtyord * quitem_qty_invuomratio) *
-                            (quitem_price / quitem_price_invuomratio), 2)), 0.0),
-         COALESCE(SUM(ROUND((quitem_qtyord * quitem_qty_invuomratio) *
-                            (quitem_unitcost / quitem_price_invuomratio), 2)), 0.0)
-         INTO _subtotal, _cost
-  FROM quitem
-  WHERE (quitem_quhead_id=pQuheadid);
-
-  IF (pType IN ('T', 'X')) THEN
-    SELECT COALESCE(SUM(tax), 0.0) INTO _tax
-    FROM (SELECT COALESCE(ROUND(SUM(taxdetail_tax), 2), 0.0) AS tax
-          FROM tax
-               JOIN calculateTaxDetailSummary('Q', pQuheadid, 'T')ON (taxdetail_tax_id=tax_id)
-          GROUP BY tax_id) AS data;
-  END IF;
-
-  IF (pType = 'T') THEN
-    SELECT COALESCE(quhead_freight, 0), COALESCE(quhead_misc, 0) INTO _freight, _misc
-    FROM quhead
-    WHERE (quhead_id=pQuheadid);
-  END IF;
-
-  _amount := CASE pType WHEN 'S' THEN (_subtotal)
-                        WHEN 'T' THEN (_subtotal + _tax + _freight + _misc)
-                        WHEN 'X' THEN (_tax)
-                        WHEN 'M' THEN (_subtotal - _cost)
-                        ELSE 0.0
-             END;
-
-  RETURN _amount;
+  RETURN calcQuoteAmt(pQuheadid, 'T', NULL, NULL, NULL, NULL, NULL, FALSE);
 
 END;
 $$ LANGUAGE 'plpgsql';
@@ -72,7 +30,7 @@ END;
 $$ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION calcQuoteAmt(pQuheadid INTEGER,
-                                        pType TEXT, pTaxzoneId INTEGER, pOrderDate DATE, pCurrId INTEGER, pFreight NUMERIC, pMisc NUMERIC) RETURNS NUMERIC STABLE AS $$
+                                        pType TEXT, pTaxzoneId INTEGER, pOrderDate DATE, pCurrId INTEGER, pFreight NUMERIC, pMisc NUMERIC, pQuick BOOLEAN DEFAULT TRUE) RETURNS NUMERIC STABLE AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
@@ -102,12 +60,22 @@ BEGIN
     SELECT COALESCE(SUM(tax), 0.0) INTO _tax
     FROM (SELECT COALESCE(ROUND(SUM(taxdetail_tax), 2), 0.0) AS tax
           FROM tax
-               JOIN calculateTaxDetailSummary('Q', pQuheadid, 'T', COALESCE(pTaxzoneId, -1), pOrderDate, pCurrId, COALESCE(pFreight,0.0))ON (taxdetail_tax_id=tax_id)
+               JOIN calculateTaxDetailSummary('Q', pQuheadid, 'T', COALESCE(pTaxzoneId, -1), pOrderDate, pCurrId, COALESCE(pFreight,0.0), pQuick) ON (taxdetail_tax_id=tax_id)
           GROUP BY tax_id) AS data;
   END IF;
 
+  IF (pQuick) THEN
+    SELECT COALESCE(pFreight, 0), COALESCE(pMisc, 0) INTO _freight, _misc;
+  ELSE
+    IF (pType = 'T') THEN
+      SELECT COALESCE(quhead_freight, 0), COALESCE(quhead_misc, 0) INTO _freight, _misc
+      FROM quhead
+      WHERE (quhead_id=pQuheadid);
+    END IF;
+  END IF;
+
   _amount := CASE pType WHEN 'S' THEN (_subtotal)
-                        WHEN 'T' THEN (_subtotal + _tax + COALESCE(pFreight, 0) + COALESCE(pMisc, 0))
+                        WHEN 'T' THEN (_subtotal + _tax + _freight + _misc)
                         WHEN 'X' THEN (_tax)
                         WHEN 'M' THEN (_subtotal - _cost)
                         ELSE 0.0
